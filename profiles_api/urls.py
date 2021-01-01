23eb50da61c82bbaa955958d51540b646dcb5ab6
jcarmonas <jcarmonas@unal.edu.co>
2021-01-01 10:45:58 -0500

    Added HelloApiView

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,5 +1,4 @@
-"""profiles_project URL Configuration
-
+"""
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
@@ -13,10 +12,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from profiles_api import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('profiles_api.urls')),
+    path('hello-view', views.HelloApiView().as_view(), name='hello_view'),
 ]
